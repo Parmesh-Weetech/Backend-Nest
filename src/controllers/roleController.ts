fb8b283a467eadfc29dbf6c243dafb437397499e
Parmesh Bhatt <parmesh@weetechsolution.com>
2025-12-23 17:50:51 +0530

    update role and user containing that role

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { Role } from "../models/roleModel.ts";
 import { Permission } from "../models/permissionModel.ts";
+import User from "../models/userModel.ts";
 
 export const createRole = async (req: Request, res: Response) => {
     try {
@@ -12,10 +13,10 @@ export const createRole = async (req: Request, res: Response) => {
             res.status(400).json({ message: "role already exists!" });
         }
 
-        const permission: boolean = await permissionIds.map(async (permission: string) => {
-            const isExists = await Permission.findOne({ permission });
+        const permission: boolean = await permissionIds.map(async (permissionId: string) => {
+            const permissionData = await Permission.findOne({ _id: permissionId });
 
-            if (!isExists) {
+            if (!permissionData) {
                 return false;
             }
 
@@ -23,7 +24,7 @@ export const createRole = async (req: Request, res: Response) => {
         });
 
         if (!permission) {
-            res.status(400).json({ message: "Permission does not exists in database" });
+            res.status(400).json({ message: `Permission does not exists in database` });
         }
 
         const role = new Role({
@@ -33,7 +34,7 @@ export const createRole = async (req: Request, res: Response) => {
 
         await role.save();
 
-        res.status(201).json({ message: "Role created successfully.", role: role});
+        res.status(201).json({ message: "Role created successfully.", role: role });
     } catch (error: any) {
         console.log(error);
         throw new Error(error);
@@ -42,7 +43,30 @@ export const createRole = async (req: Request, res: Response) => {
 
 export const updateRole = async (req: Request, res: Response) => {
     try {
+        const { id, name, permissionIds } = req.body;
 
+        const permission: boolean = await permissionIds.map(async (permissionId: string) => {
+            const permissionData = await Permission.findOne({ _id: permissionId });
+
+            if (!permissionData) {
+                return false;
+            }
+
+            return true;
+        });
+
+        if (!permission) {
+            res.status(400).json({ message: `One / Many Permissions does not exists in database.` });
+        }
+
+        const newRole = Role.findByIdAndUpdate(id, {
+            name,
+            permissionIds: permissionIds
+        }, {
+            new: true
+        })
+
+        res.status(200).json({ message: "Role updated successfully", role: newRole });
     } catch (error: any) {
         console.log(error);
         throw new Error(error);
@@ -51,7 +75,33 @@ export const updateRole = async (req: Request, res: Response) => {
 
 export const deleteRole = async (req: Request, res: Response) => {
     try {
+        const id = req.params.id;
 
+        if (!id) {
+            res.status(400).json({ message: "Role id is required in order to perform this action " });
+        }
+
+        const role = await Role.findOne({ _id: id });
+
+        if (!role) {
+            res.status(400).json({ message: "Role doesn't exists in database" });
+        }
+
+        role?.permissionsIds.map(async (permissionId) => {
+            await Permission.findByIdAndDelete(permissionId);
+        })
+
+        await Role.findByIdAndDelete(id);
+
+        const users = await User.find({ roleId: id! });
+
+        users.map(async (user) => {
+            await User.findByIdAndUpdate(user._id, {
+                roleId: null
+            })
+        })
+
+        res.status(200).json({ message: "Role deleted successfully " });
     } catch (error: any) {
         console.log(error);
         throw new Error(error);
