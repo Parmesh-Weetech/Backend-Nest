2ca1dcf6f0048919d9d34b99e63f13960a2dfc95
Parmesh Bhatt <parmesh@weetechsolution.com>
2026-01-12 10:09:42 +0530

    refactor: changed error response status code from 400 to 401

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -35,7 +35,7 @@ export class AuthController {
     async login(@Body() loginDTO: LoginDTO, @Res({ passthrough: true }) res: Response): Promise<void> {
         const response = await this.authService.login(loginDTO);
 
-        if(!response.success) res.status(400).json({ success: false, message: "Invalid credentials!"})
+        if(!response.success) res.status(401).json({ success: false, message: "Invalid credentials!"})
 
         res.status(200).send(response);
     }
