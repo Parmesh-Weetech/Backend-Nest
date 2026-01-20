a03655a169733acae5906d659bb6d9af69976567
Parmesh Bhatt <parmesh@weetechsolution.com>
2026-01-20 11:13:24 +0530

    refactor: added headers checking logic in organization module

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -12,35 +12,35 @@ export class OrganizationController {
 
   @Get()
   async findAll(@Res({ passthrough: true }) res: Response, @Headers("Authorization") authorization: string): Promise<void> {
-    const response = await this.organizationService.findAll();
+    const response = await this.organizationService.findAll(authorization);
 
     res.status(response.statusCode).send(response);
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string, @Res({ passthrough: true }) res: Response, @Headers("Authorization") authorization: string): Promise<void> {
-    const response = await this.organizationService.findOne(id);
+    const response = await this.organizationService.findOne(id, authorization);
 
     res.status(response.statusCode).send(response);
   }
 
   @Post()
-  async create(@Body() createOrganizationDto: CreateOrganizationDto, @Res({ passthrough: true }) res: Response): Promise<void> {
-    const response = await this.organizationService.create(createOrganizationDto);
+  async create(@Body() createOrganizationDto: CreateOrganizationDto, @Res({ passthrough: true }) res: Response, @Headers("Authorization") authorization: string): Promise<void> {
+    const response = await this.organizationService.create(createOrganizationDto, authorization);
 
     res.status(response.statusCode).send(response);
   }
 
   @Put()
-  async update(@Body() updateOrganizationDto: UpdateOrganizationDto, @Res({ passthrough: true }) res: Response): Promise<void> {
-    const response = await this.organizationService.update(updateOrganizationDto);
+  async update(@Body() updateOrganizationDto: UpdateOrganizationDto, @Res({ passthrough: true }) res: Response, @Headers("Authorization") authorization: string): Promise<void> {
+    const response = await this.organizationService.update(updateOrganizationDto, authorization);
 
     res.status(response.statusCode).send(response);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string, @Res({ passthrough: true }) res: Response): Promise<void> {
-    const response = await this.organizationService.remove(id);
+  async remove(@Param('id') id: string, @Res({ passthrough: true }) res: Response, @Headers("Authorization") authorization: string): Promise<void> {
+    const response = await this.organizationService.remove(id, authorization);
 
     res.status(response.statusCode).send(response);
   }
--- src/organization/organization.service.ts
+++ src/organization/organization.service.ts
@@ -16,7 +16,40 @@ export class OrganizationService {
     @Inject(forwardRef(() => UserService))
     private readonly userService: UserService
   ) { }
-  async findAll(): Promise<Response> {
+  async findAll(authorization: string): Promise<Response> {
+    const [type, token] = authorization?.split(' ') ?? [];
+    const access_token = type === 'Bearer' ? token : undefined;
+
+    if (!access_token) return {
+      success: false,
+      message: "Token is required",
+      data: null,
+      expired: false,
+      statusCode: 401
+    }
+
+    const isValid = await this.auth.verify(access_token);
+
+    if (!isValid) return {
+      success: false,
+      message: "Token expired!",
+      data: null,
+      expired: true,
+      statusCode: 400
+    }
+
+    const decodedPayload = await this.auth.decode(access_token);
+
+    const isUserExists = await this.userService.findOne(decodedPayload.sub);
+
+    if (!isUserExists) return {
+      success: false,
+      message: "User not found!",
+      data: null,
+      expired: false,
+      statusCode: 404
+    }
+
     const organizations = await this.organizationRepository.find();
 
     if (!organizations) return {
@@ -36,7 +69,40 @@ export class OrganizationService {
     };
   }
 
-  async findOne(id: string): Promise<Response> {
+  async findOne(id: string, authorization: string): Promise<Response> {
+    const [type, token] = authorization?.split(' ') ?? [];
+    const access_token = type === 'Bearer' ? token : undefined;
+
+    if (!access_token) return {
+      success: false,
+      message: "Token is required",
+      data: null,
+      expired: false,
+      statusCode: 401
+    }
+
+    const isValid = await this.auth.verify(access_token);
+
+    if (!isValid) return {
+      success: false,
+      message: "Token expired!",
+      data: null,
+      expired: true,
+      statusCode: 400
+    }
+
+    const decodedPayload = await this.auth.decode(access_token);
+
+    const isUserExists = await this.userService.findOne(decodedPayload.sub);
+
+    if (!isUserExists) return {
+      success: false,
+      message: "User not found!",
+      data: null,
+      expired: false,
+      statusCode: 404
+    }
+
     const organization = await this.organizationRepository.findOne({ where: { id: id } });
 
     if (!organization) return {
@@ -56,8 +122,41 @@ export class OrganizationService {
     };
   }
 
-  async create(createOrganizationDto: CreateOrganizationDto): Promise<Response> {
+  async create(createOrganizationDto: CreateOrganizationDto, authorization: string): Promise<Response> {
     try {
+      const [type, token] = authorization?.split(' ') ?? [];
+      const access_token = type === 'Bearer' ? token : undefined;
+
+      if (!access_token) return {
+        success: false,
+        message: "Token is required",
+        data: null,
+        expired: false,
+        statusCode: 401
+      }
+
+      const isValid = await this.auth.verify(access_token);
+
+      if (!isValid) return {
+        success: false,
+        message: "Token expired!",
+        data: null,
+        expired: true,
+        statusCode: 400
+      }
+
+      const decodedPayload = await this.auth.decode(access_token);
+
+      const isUserExists = await this.userService.findOne(decodedPayload.sub);
+
+      if (!isUserExists) return {
+        success: false,
+        message: "User not found!",
+        data: null,
+        expired: false,
+        statusCode: 404
+      }
+
       const newOrganization = await this.organizationRepository.create(createOrganizationDto);
 
       const organization = await this.organizationRepository.save(newOrganization);
@@ -80,8 +179,41 @@ export class OrganizationService {
     }
   }
 
-  async update(updateOrganizationDto: UpdateOrganizationDto): Promise<Response> {
-    const existingOrganization = await this.findOne(updateOrganizationDto.id);
+  async update(updateOrganizationDto: UpdateOrganizationDto, authorization: string): Promise<Response> {
+    const [type, token] = authorization?.split(' ') ?? [];
+    const access_token = type === 'Bearer' ? token : undefined;
+
+    if (!access_token) return {
+      success: false,
+      message: "Token is required",
+      data: null,
+      expired: false,
+      statusCode: 401
+    }
+
+    const isValid = await this.auth.verify(access_token);
+
+    if (!isValid) return {
+      success: false,
+      message: "Token expired!",
+      data: null,
+      expired: true,
+      statusCode: 400
+    }
+
+    const decodedPayload = await this.auth.decode(access_token);
+
+    const isUserExists = await this.userService.findOne(decodedPayload.sub);
+
+    if (!isUserExists) return {
+      success: false,
+      message: "User not found!",
+      data: null,
+      expired: false,
+      statusCode: 404
+    }
+
+    const existingOrganization = await this.findOne(updateOrganizationDto.id, authorization);
 
     if (existingOrganization.data.name) existingOrganization.data.name = updateOrganizationDto.name;
 
@@ -106,9 +238,42 @@ export class OrganizationService {
     }
   }
 
-  async remove(id: string): Promise<Response> {
+  async remove(id: string, authorization): Promise<Response> {
     try {
-      const organization = await this.findOne(id);
+      const [type, token] = authorization?.split(' ') ?? [];
+      const access_token = type === 'Bearer' ? token : undefined;
+
+      if (!access_token) return {
+        success: false,
+        message: "Token is required",
+        data: null,
+        expired: false,
+        statusCode: 401
+      }
+
+      const isValid = await this.auth.verify(access_token);
+
+      if (!isValid) return {
+        success: false,
+        message: "Token expired!",
+        data: null,
+        expired: true,
+        statusCode: 400
+      }
+
+      const decodedPayload = await this.auth.decode(access_token);
+
+      const isUserExists = await this.userService.findOne(decodedPayload.sub);
+
+      if (!isUserExists) return {
+        success: false,
+        message: "User not found!",
+        data: null,
+        expired: false,
+        statusCode: 404
+      }
+      
+      const organization = await this.findOne(id, authorization);
 
       if (!organization.success) return organization;
 
