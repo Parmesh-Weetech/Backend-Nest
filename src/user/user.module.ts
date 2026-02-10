24ce57d57eb21ea500501d0cf7418e288fe8f952
Parmesh Bhatt <parmesh@weetechsolution.com>
2026-02-10 18:11:29 +0530

    refactor: remove unwanted comment lines

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,6 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
 import { RoleModule } from '../role/role.module';
 import { PermissionModule } from '../permission/permission.module';
